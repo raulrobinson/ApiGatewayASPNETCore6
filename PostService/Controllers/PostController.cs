e02ad058fe0d4181edbcefd41782d66cd56c7075
rasysbox <71100949+raulrobinson@users.noreply.github.com>
2023-04-03 22:39:37 -0500

    PostService

---
+++ PostService/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PostService.Dtos;
+
+namespace PostService.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class PostController : ControllerBase
+    {
+        private readonly ILogger<PostController> _logger;
+
+        public PostController(ILogger<PostController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("posts")]
+        public async Task<IActionResult> Get()
+        {
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var postList = JsonConvert.DeserializeObject<List<PostDto>>(apiResponse);
+
+            return Ok(postList);
+        }
+
+        [HttpGet("posts/{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync($"https://jsonplaceholder.typicode.com/posts/{id}");
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var post = JsonConvert.DeserializeObject<PostDto>(apiResponse);
+
+            return Ok(post);
+        }
+    }
+}
---
+++ PostService/Dtos/PostDto.cs
@@ -0,0 +1,15 @@
+﻿namespace PostService.Dtos
+{
+    public class PostDto
+    {
+        public long UserId { get; set; }
+
+        public long Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Body { get; set; }
+
+        //public List<CommentDto> comments = new();
+    }
+}
