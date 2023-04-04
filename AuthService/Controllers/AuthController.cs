b5b786086d0d011b13a51280c812c569b6bdb82b
rasysbox <71100949+raulrobinson@users.noreply.github.com>
2023-04-03 22:47:02 -0500

    AuthService

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Obtiene token para usar en API Gateway
+        /// username: user or admin
+        /// registered: no-registered or registered
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="registered"></param>
+        /// <returns></returns>
+        [HttpGet("auth/token")]
+        public IActionResult Get(string userName, string userType)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("213GJQWJHGJ23G78GJQAWKH2L3JV452CVHVJV1J2VJAGSDJCVBZMMQWEJB2J3423J4VJ2V4J2MM");
+            var issuer = "rasysbox.com";
+            var audience = "Public";
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userName),
+                new Claim("UserType", userType)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = issuer,
+                Audience = audience,
+                Subject = new ClaimsIdentity(claims.ToArray()),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(tokenHandler.WriteToken(token));
+        }
+    }
+}
