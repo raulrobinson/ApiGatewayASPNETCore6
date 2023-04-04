8ae58972d1358c8fd391d0ac0543c0fbdbe2372d
rasysbox <71100949+raulrobinson@users.noreply.github.com>
2023-04-03 22:33:50 -0500

    UserService

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UserService.Dtos;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("users")]
+        public async Task<IActionResult> Get()
+        {
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var userList = JsonConvert.DeserializeObject<List<UserDto>>(apiResponse);
+
+            return Ok(userList);
+        }
+
+        [HttpGet("users/{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync($"https://jsonplaceholder.typicode.com/users/{id}");
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var post = JsonConvert.DeserializeObject<UserDto>(apiResponse);
+
+            return Ok(post);
+        }
+
+    }
+}
